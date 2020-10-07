8f269c2b1250fae6736041746d55f10010909fdb
maximillian <makcmakc321@mail.ru>
2020-10-07 23:15:33 +0300

    Create

--- src/cli.js
+++ src/cli.js
@@ -1,1 +1,94 @@
- 
+const arg = require('arg')
+const inquirer = require('inquirer')
+const { createProject } = require('./main')
+
+
+function parseArgumentsIntoOptions(raw) {
+  const args = arg(
+    {
+      '--git': Boolean,
+      '--yes': Boolean,
+      '--install': Boolean,
+      '-g': '--git',
+      '-y': '--yes'
+    },
+    {
+      argv: raw.slice(2)
+    }
+  )
+
+  return {
+    skipPrompts: args['--yes'] || false,
+    git: args['--git'] || false,
+    template: args._[0],
+    // function: args._[0],
+    runInstall: args['--install'] || false
+  }
+}
+
+async function promptForMissingOptions(options) {
+  const defaultTemplate = 'Default'
+
+  if (options.skipPrompts) {
+    return {
+      ...options,
+      template: options.template || defaultTemplate
+    }
+  }
+
+  const questions = []
+
+  if (!options.name) {
+    questions.push({
+      type: 'input',
+      name: 'name',
+      message: 'What’s the name of your project?',
+      default: 'My Awesome App'
+    })
+  }
+  if (!options.webpack) {
+    questions.push({
+      type: 'confirm',
+      name: 'webpack',
+      message: 'Use webpack dev server (hot reload)?',
+      default: true
+    })
+  }
+  if (!options.template) {
+    questions.push({
+      type: 'list',
+      name: 'template',
+      message: 'Please choose which project template to use',
+      choices: ['Default', 'JavaScript', 'TypeScript'],
+      default: defaultTemplate
+    })
+  }
+
+  if (!options.git) {
+    questions.push({
+      type: 'confirm',
+      name: 'git',
+      message: 'Initialize a git repository?',
+      default: true
+    })
+  }
+
+  const answers = await inquirer.prompt(questions)
+
+  return {
+    ...options,
+    name: options.name || answers.name,
+    template: options.template || answers.template,
+    webpack: options.webpack || answers.webpack,
+    git: options.git || answers.git
+  } 
+}
+
+
+export async function cli(args) {
+  let options = parseArgumentsIntoOptions(args)
+  options = await promptForMissingOptions(options)
+  // console.log(options)
+
+  await createProject(options)
+}
---
+++ src/main.js
@@ -0,0 +1,53 @@
+const chalk = require('chalk')
+const fs = require('fs')
+const ncp = require('ncp')
+const path = require('path')
+const { promisify } = require('util')
+const execa = require('execa')
+const Listr = require('listr')
+const { projectInstall } = require('pkg-install')
+
+const access = promisify(fs.access)
+const copy = promisify(ncp)
+
+async function copyTemplateFiles(options) {
+  return copy(options.templateDirectory, options.targetDirectory, {
+    clobber: false
+  })
+}
+
+export async function createProject(options) {
+  const cwd = options.cwd || process.cwd()
+  const inCurrent = options.name === '.'
+  const name = inCurrent ? path.relative('../', cwd) : options.name
+  const targetDir = path.resolve(cwd, options.name || '.')
+
+  options = {
+    ...options,
+    targetDirectory: options.targetDirectory || targetDir
+  }
+
+ const templateDir = path.resolve(
+   new URL(import.meta.url).pathname,
+   '../../templates',
+   options.template
+ );
+ options.templateDirectory = templateDir
+
+ try {
+   await access(templateDir, fs.constants.R_OK)
+ } catch (err) {
+   console.error('%s Invalid template name', chalk.red.bold('ERROR'))
+   process.exit(1)
+ }
+
+ console.log('Copy project files')
+ await copyTemplateFiles(options)
+
+ console.log(`🎉  Successfully created project ${chalk.yellow(name)}.`)
+
+ return true
+
+}
+
+
